50f1110a5c61c71a0ae1b75151df69aa61a1e756
Alexander Tolkachev <alexandr.tolkachyov@gmail.com>
2024-10-04 17:42:19 +0800

    done homework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Подключаем CSS файл
 
 function App() {
+  const [isClicked, setIsClicked] = useState(false);
+  const [clickCount, setClickCount] = useState(0);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+    setClickCount(clickCount + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={clickCount % 2 === 0 ? 'App even' : 'App odd'}>
+      <button 
+        className={isClicked ? 'clicked' : 'unclicked'} 
+        onClick={handleClick}
+      >
+        Click me!
+      </button>
     </div>
   );
 }
 
 export default App;
+
